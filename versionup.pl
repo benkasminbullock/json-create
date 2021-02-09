f39cae93d94d822269a7c52f5ebe419160adc7a1
Ben Bullock <benkasminbullock@gmail.com>
2021-02-09 22:31:43 +0900

    Version to 0.32_03

--- versionup.pl
+++ versionup.pl
@@ -10,8 +10,8 @@ use FindBin '$Bin';
 use File::Slurper qw!read_text write_text!;
 use Deploy 'make_date';
 
-my $version = '0.30_01';
-my $newversion = '0.30_02';
+my $version = '0.30_02';
+my $newversion = '0.30_03';
 
 my @pmfiles = qw!
 lib/JSON/Create.pm
